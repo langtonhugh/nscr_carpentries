904dab4f6f0f24886b301281ab41b7fcaa59de3d
Sam Langton <36928709+langtonhugh@users.noreply.github.com>
2022-09-12 21:35:18 +0200

    long wide

--- rmarkdown/rmarkdown.R
+++ rmarkdown/rmarkdown.R
@@ -1,25 +1,22 @@
 # Use this script to render the rmarkdown files and save them in
 # the worksheets folder.
 
-# Load libraries.
-library(rmarkdown)
-
 # Render and save in the worksheets folder.
-render(input       = "rmarkdown/slides.Rmd", 
-       output_dir  = "worksheets",
-       output_file = "slides.html")
+rmarkdown::render(input       = "rmarkdown/slides.Rmd", 
+                  output_dir  = "worksheets",
+                  output_file = "slides.html")
 
-render(input       = "rmarkdown/day1_worksheet.Rmd",
-       output_dir  = "worksheets",
-       output_file = "day1_worksheet.html")
+rmarkdown::render(input       = "rmarkdown/day1_worksheet.Rmd",
+                  output_dir  = "worksheets",
+                  output_file = "day1_worksheet.html")
 
-render(input       = "rmarkdown/day2_morning_worksheet.Rmd",
-       output_dir  = "worksheets",
-       output_file = "day2_morning_worksheet.html")
+rmarkdown::render(input       = "rmarkdown/day2_morning_worksheet.Rmd",
+                  output_dir  = "worksheets",
+                  output_file = "day2_morning_worksheet.html")
 
-render(input       = "rmarkdown/day2_afternoon_worksheet.Rmd",
-       output_dir  = "worksheets",
-       output_file = "day2_afternoon_worksheet.html")
+rmarkdown::render(input       = "rmarkdown/day2_afternoon_worksheet.Rmd",
+                  output_dir  = "worksheets",
+                  output_file = "day2_afternoon_worksheet.html")
 
 
 
